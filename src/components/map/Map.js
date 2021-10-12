097c30d95f89fe60199f6f1eca42cb43d2d52945
Steve Suthakaran <stevesuthakaran@gmail.com>
2021-10-12 19:02:27 +0100

    updated packages and fixed bugs

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -10,93 +10,117 @@ import camera from "../../assets/camera.png";
 import activity from "../../assets/activity.png";
 
 let mapStyles;
-  if(window.matchMedia("(max-width: 500px)").matches) {
-    mapStyles = {
-      width: "95vw",
-      height: "50vh",
-      margin: "10px"
-      
-    }
-  } else {
-    mapStyles = {
-      width: "65vw",
-      height: "70vh",
-      margin: "auto"
-    }
-  }
+if (window.matchMedia("(max-width: 500px)").matches) {
+  mapStyles = {
+    width: "95vw",
+    height: "50vh",
+    margin: "10px",
+  };
+} else {
+  mapStyles = {
+    width: "65vw",
+    height: "70vh",
+    margin: "auto",
+  };
+}
 
-class MapContainer extends Component{
-  state = {mapLoaded : false}
+class MapContainer extends Component {
+  state = { mapLoaded: false };
 
   shouldComponentUpdate(nextProps, nextState) {
     return true;
   }
 
   iconFunc(type) {
-    if(type === "Activities") return {url: activity, scaledSize: new window.google.maps.Size(50, 50)}
-    if(type === "Clubbing") return {url: clubbing, scaledSize: new window.google.maps.Size(50, 50)}
-    if(type === "Shopping") return {url: shopping, scaledSize: new window.google.maps.Size(50, 50)}
-    if(type === "Sightseeing") return {url: camera, scaledSize: new window.google.maps.Size(50, 50)}
-    if(type === "Restaurant") return {url: plate, scaledSize: new window.google.maps.Size(50, 50)}
+    if (type === "Activities")
+      return { url: activity, scaledSize: new window.google.maps.Size(50, 50) };
+    if (type === "Clubbing")
+      return { url: clubbing, scaledSize: new window.google.maps.Size(50, 50) };
+    if (type === "Shopping")
+      return { url: shopping, scaledSize: new window.google.maps.Size(50, 50) };
+    if (type === "Sightseeing")
+      return { url: camera, scaledSize: new window.google.maps.Size(50, 50) };
+    if (type === "Restaurant")
+      return { url: plate, scaledSize: new window.google.maps.Size(50, 50) };
   }
-  
+
   defaultCenter = {
-    lat: 51.546983, lng: -0.146625
-  }
+    lat: 51.546983,
+    lng: -0.146625,
+  };
 
   allCenter = {
-    lat: 51.508638, lng: -0.116929
-  }
+    lat: 51.508638,
+    lng: -0.116929,
+  };
 
-  render () {    
+  render() {
     const { location, time, price, selectAll } = this.props.filterData;
     const typeState = this.props.legend;
 
     const priceArray = (price) => {
-      let priceFilter = parseInt(price, 10)
-      if(priceFilter === 1) return [1]
-      if(priceFilter === 2) return [1,2]
-      if(priceFilter === 3) return [1,2,3]
-    }
+      let priceFilter = parseInt(price, 10);
+      if (priceFilter === 1) return [1];
+      if (priceFilter === 2) return [1, 2];
+      if (priceFilter === 3) return [1, 2, 3];
+    };
 
     return (
       <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={selectAll ? 10 : 11}
-          center={selectAll ? this.allCenter : location ? location[1] : this.defaultCenter}
-          onLoad={() => this.setState({mapLoaded: true})}
+          center={
+            selectAll
+              ? this.allCenter
+              : location
+              ? location[1]
+              : this.defaultCenter
+          }
+          onLoad={() => this.setState({ mapLoaded: true })}
         >
-          {
-            this.state.mapLoaded && data
-              .filter(arr => selectAll ? arr : arr.id.includes(location && location[0]))
-              .filter(arr => selectAll ? arr : arr.time.includes(!time ? "Day" : "Night"))
-              .filter(arr => selectAll ? arr : priceArray(price).some(el => arr.price.includes(el)))
-                .map((item) => (
-                <Marker 
-                  
-                  opacity={typeState && (typeState[item.type] === item.type) ? 0 : 1}
+          {this.state.mapLoaded &&
+            data
+              .filter((arr) =>
+                selectAll ? arr : arr.id.includes(location && location[0])
+              )
+              .filter((arr) =>
+                selectAll ? arr : arr.time.includes(!time ? "Day" : "Night")
+              )
+              .filter((arr) =>
+                selectAll
+                  ? arr
+                  : priceArray(price).some((el) => arr.price.includes(el))
+              )
+              .map((item) => (
+                <Marker
+                  opacity={
+                    typeState && typeState[item.type] === item.type ? 0 : 1
+                  }
                   icon={this.iconFunc(item.type)}
                   onMouseOver={() => {
-                    typeState && (item.type !== typeState[item.type]) && this.props.getId(item.id)
+                    typeState &&
+                      item.type !== typeState[item.type] &&
+                      this.props.getId(item.id);
                   }}
                   onMouseDown={() => {
-                    typeState && (item.type !== typeState[item.type]) && this.props.getId(item.id)
+                    typeState &&
+                      item.type !== typeState[item.type] &&
+                      this.props.getId(item.id);
                   }}
-                  key={item.id} 
-                  position={item.position} 
-                  animation={window.google.maps.Animation.DROP} 
+                  key={item.id}
+                  position={item.position}
+                  animation={window.google.maps.Animation.DROP}
                 />
-              ))
-          }
+              ))}
         </GoogleMap>
       </LoadScript>
-    )     
-  };
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-  return {legend: state.legendReducer}
-}
+  return { legend: state.legendReducer };
+};
 
 export default connect(mapStateToProps, null)(MapContainer);
--- src/components/navbarInfo/NavbarInfo.js
+++ src/components/navbarInfo/NavbarInfo.js
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
 import { useHistory } from "react-router-dom";
-import "./NavbarInfo.css";  
+import "./NavbarInfo.css";
 
 import { useDispatch } from "react-redux";
 import { legendFunc } from "../../store/actions/legendActions";
@@ -16,74 +16,80 @@ import moon from "../../assets/moon.png";
 import Pound from "../../assets/pound";
 
 const NavbarInfo = ({ filterData }) => {
-
-  const {location, time, price, selectAll} = filterData
+  const { location, time, price, selectAll } = filterData;
   const history = useHistory();
 
   const locationFunc = () => {
-    if(location[0] === "North") return <North />
-    if(location[0] === "East") return <East />
-    if(location[0] === "South") return <South />
-    if(location[0] === "West") return <West />
-    if(location[0] === "Central") return <Central />
-  }
+    if (location[0] === "North") return <North />;
+    if (location[0] === "East") return <East />;
+    if (location[0] === "South") return <South />;
+    if (location[0] === "West") return <West />;
+    if (location[0] === "Central") return <Central />;
+  };
 
   const dispatch = useDispatch();
 
-  return(
-    <div className="navbar" >
+  return (
+    <div className="navbar">
       <ul>
         <li>
-          <span className="navbar-span" >
-            {selectAll 
-              ? <>
-                  <span style={{marginRight: "10px"}} >All areas</span>
-                  <London navbar={true} selectAll={true} />
-                </>
-              : <>
-                  <span style={{marginRight: "10px"}} >{location[0]}</span> 
-                  {locationFunc()}
-                </>
-            }
+          <span className="navbar-span">
+            {selectAll ? (
+              <>
+                <span className="allAreas">All areas</span>
+                <London navbar={true} selectAll={true} />
+                <span> , </span>
+              </>
+            ) : (
+              <>
+                <span className="oneArea">{location[0]}</span>
+                {locationFunc()}
+                <span> , </span>
+              </>
+            )}
           </span>
         </li>
-        {!selectAll && <li className="gap-text" >at</li>}
+        {!selectAll && <li className="gap-text">at</li>}
         <li>
-          {selectAll 
-          ? <>
-              <span className="navbar-span" >
-                <img alt="time" src={sun} /> 
-                <div style={{margin: "10px"}} >{"&"}</div>
+          {selectAll ? (
+            <>
+              <span className="navbar-span">
+                <img alt="time" src={sun} />
+                <div className="and">{"&"}</div>
                 <img alt="time" src={moon} />
+                <span> , </span>
               </span>
             </>
-          : <>
-              <span className="navbar-span" >
-                <img alt="time" src={!time ? sun : moon} /> 
+          ) : (
+            <>
+              <span className="navbar-span">
+                <img alt="time" src={!time ? sun : moon} />
                 time
+                <span> , </span>
               </span>
             </>
-          }
+          )}
         </li>
-        <li className="gap-text" >up to</li>
+        <li className="gap-text">up to</li>
         <li>
           <span>
             <Pound value={price} />
-            <Pound selectAll={selectAll} value={price > 1  && price} />
+            <Pound selectAll={selectAll} value={price > 1 && price} />
             <Pound selectAll={selectAll} value={price > 2 && price} />
           </span>
         </li>
         <li>
-          <button 
+          <button
             onClick={() => {
-              history.push("/")
-              dispatch(legendFunc("reset"))
-            }} 
-          >Home
-        </button>
+              history.push("/");
+              dispatch(legendFunc("reset"));
+            }}
+          >
+            Home
+          </button>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 export default memo(NavbarInfo);
